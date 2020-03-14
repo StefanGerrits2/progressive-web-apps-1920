df79160483d552f66f361e610aa00204b1c892d0
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-03-14 11:20:30 +0100

    remove unneeded favicon

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const favicon = require('serve-favicon');
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
@@ -20,7 +19,6 @@ app
         partialsDir: __dirname + '/views/partials/'
     }))
     .use('/', express.static(publicPath))
-    .use(favicon(path.join(__dirname, 'public', 'img', 'favicon.png')))
 
     // Get routes
     .get('/', home)
