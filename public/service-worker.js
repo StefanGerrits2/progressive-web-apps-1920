2daa0218a9a948f2a94247f9d3feba0188e0d825
Stefan Gerrits <stefangerrits2000@gmail.com>
2020-03-17 16:26:43 +0100

    cache images

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,14 @@
 const CORE_CACHE = 1;
 const CORE_CACHE_NAME = `core-cache-v${CORE_CACHE}`; 
 const CORE_ASSETS = [
-    '/',
     '/offline',
     '/dist/styles.css', 
-    '/img/header-logo.png'
+    '/img/header-logo.png',
 ]; 
 
-self.addEventListener('install', e => {   
+self.addEventListener('install', event => {   
     console.log('Service worker install event!');  
-    e.waitUntil(caches.open(CORE_CACHE_NAME)             
+    event.waitUntil(caches.open(CORE_CACHE_NAME)             
         .then(cache => cache.addAll(CORE_ASSETS))             
         .then(() => self.skipWaiting())     
     ); 
@@ -21,8 +20,9 @@ self.addEventListener('activate', event => {
 });
   
 self.addEventListener('fetch', event => {
-    console.log('Fetch event: ', event.request.url);
+    console.log('Fetch event: ', event.request);
     
+    // Cache cores
     if (isCoreGetRequest(event.request)) {
         console.log('Core get request: ', event.request.url);
         // cache only strategy
@@ -30,7 +30,7 @@ self.addEventListener('fetch', event => {
             caches.open(CORE_CACHE_NAME)
                 .then(cache => cache.match(event.request.url))
         );
-
+    // Cache HTML
     } else if (isHtmlGetRequest(event.request)) {
         console.log('html get request', event.request.url);
         // generic fallback
@@ -43,6 +43,18 @@ self.addEventListener('fetch', event => {
                         .then(cache => cache.match('/offline'));
                 })
         );
+    // Cache images
+    } else if (isImageGetRequest(event.request)) {
+        console.log('img get request', event.request.url);
+        // generic fallback
+        event.respondWith(
+            caches.open('img-cache')
+                .then(cache => cache.match(event.request.url))
+                .then(response => response ? response : fetchAndCache(event.request.url, 'img-cache'))
+                .catch(e => {
+                    console.log(e);
+                })
+        );
     }
 });
 
@@ -52,11 +64,15 @@ function fetchAndCache(request, cacheName) {
             if (!response.ok) {
                 throw new TypeError('Bad response status');
             }
-  
+
             const clone = response.clone();
             caches.open(cacheName).then((cache) => cache.put(request, clone));
             return response;
         });
+}
+
+function isImageGetRequest(request) {
+    return request.method === 'GET' && (request.headers.get('accept') !== null && request.url.indexOf('.png') > -1);
 }
 
 function isHtmlGetRequest(request) {
